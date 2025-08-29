f79208ed23d6f952a87c73c8eef6a58d46a13ee9
Adit Shah <102573837+CodeRockerr@users.noreply.github.com>
2025-08-29 18:44:34 -0400

    Update constants.js

--- vite-project/src/constants.js
+++ vite-project/src/constants.js
@@ -1,10 +1,9 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my Laptop. I work mostly in the fields of Data Analytics and Full Stack Web Development these days.
-    I've analysed many projects in Microsoft Power BI. I also like Python. Programming is in my blood!
+  pc: `This is my Mac. I work mostly in the fields of Full Stack Development, AI and Data Analytics these days.
     Here is my <a href="https://www.github.com/CodeRockerr" target="_blank">Github</a>!`,
-  "cs-degree": `My name is Adit Shah. These are my Social media profiles. You can check it out here and also hit that follow button! - 
+  "cs-degree": `My name is Adit Shah. I am a Master's in Computer Science Student at North Carolina State University. These are my Social media profiles. You can check it out here and also hit that follow button! - 
   <a href="https://www.linkedin.com/in/shah-adit0404/" target="_blank">LinkedIn</a>
   <a href="https://www.instagram.com/adit_4403/" target="_blank">Instagram</a>`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
@@ -13,7 +12,7 @@ export const dialogueData = {
   bed: `This is my brainstorming bed! Ideas pop like popcorn while I'm lying down sleeping. If I don't write 'em, I'll toss and turn all night!
   "This is where dreams turn into reality!"`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/CodeRockerr/Resume/blob/main/Adit_Resume.pdf" target="_blank">Check it out!</a>
-  Ping me at aditjignesh44@gmail.com if you have any interesting job/internship opportunities!`,
+  Ping me at shahadit62@gmail.com if you have any interesting Summer Internship opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching some of the tutorials from this yooutbe channels - <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
