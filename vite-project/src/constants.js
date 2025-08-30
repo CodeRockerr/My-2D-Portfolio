46127c2f9d4e7d1426f354a295ae9ba4e47a5121
Adit Shah <102573837+CodeRockerr@users.noreply.github.com>
2025-08-29 23:14:51 -0400

    Update constants.js

--- vite-project/src/constants.js
+++ vite-project/src/constants.js
@@ -11,7 +11,11 @@ export const dialogueData = {
   tv: `That's my TV. I've been watching tech youtubers a lot recently. I also watch Cricket and news related to Politics and Current-Affairs.`,
   bed: `This is my brainstorming bed! Ideas pop like popcorn while I'm lying down sleeping. If I don't write 'em, I'll toss and turn all night!
   "This is where dreams turn into reality!"`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/CodeRockerr/Resume/blob/main/Resume_Adit.pdf" target="_blank">Check it out!</a>
+  resume: `This is my desk and on it is my resume. 
+  <br/>
+  <img src="/qr.png" alt="Resume QR code" width="150" height="150" />
+  <br/>
+  // <a href="https://github.com/CodeRockerr/Resume/blob/main/Resume_Adit.pdf" target="_blank">Check it out!</a>
   Ping me at shahadit62@gmail.com if you have any interesting Summer Internship opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
