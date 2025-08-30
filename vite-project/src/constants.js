9fd860641f178db1570c5cce283311cb7c2f4683
Adit Shah <102573837+CodeRockerr@users.noreply.github.com>
2025-08-29 23:28:54 -0400

    Update constants.js

--- vite-project/src/constants.js
+++ vite-project/src/constants.js
@@ -16,8 +16,8 @@ export const dialogueData = {
   <br/>
     <img src="${resumeQr}" alt="Resume QR code" width="150" height="150" />
   <br/>
-  // <a href="https://github.com/CodeRockerr/Resume/blob/main/Resume_Adit.pdf" target="_blank">Check it out!</a>
-  Ping me at shahadit62@gmail.com if you have any interesting Summer Internship opportunities!`,
+  Scan the QR Code and find my Resume!
+  Ping me at <a href="mailto:shahadit62@gmail.com">shahadit62@gmail.com</a> if you have any interesting Summer Internship opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching some of the tutorials from this yooutbe channels - <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
